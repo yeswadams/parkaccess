496facd21f2f4de0583b558ba6f67bf377e756c9
Jeremy Mwaura <jeremy.mwaura@student.moringaschool.com>
2026-07-26 16:00:37 +0300

    created schemas for models

---
+++ app/schemas/__init__.py
@@ -0,0 +1,13 @@
+from .base import BaseSchema
+from .checkpoint_schema import CheckpointSchema
+from .checkpoint_log_schema import CheckpointLogSchema
+from .user_schema import UserSchema
+from .vehicle_schema import VehicleSchema
+
+__all__ = [
+    "BaseSchema",
+    "CheckpointSchema",
+    "CheckpointLogSchema",
+    "UserSchema",
+    "VehicleSchema",
+]
